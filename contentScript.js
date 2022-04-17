fdd5eedfb2bfe021447679a01f2130902e32b6a9
Raman Hundal <ramanhundal@Ramans-MBP.fios-router.home>
2022-04-17 16:28:59 -0400

    Add css styling.

--- contentScript.js
+++ contentScript.js
@@ -1,14 +1,10 @@
 (() => {
-  /* Fetch all bookmarks for current video */
   const fetchBookmarks = () => {};
 
-  /* Handle request to add new bookmark */
   const addNewBookmarkEventHandler = () => {};
 
-  /* this checks for whenever a new video is loaded or the current tab is reloaded */
   const newVideoLoaded = (refreshed) => {};
 
-  /* respond to messages from background or popup pages */
   chrome.runtime.onMessage.addListener((obj, sender, sendResponse) => {
     sendResponse({});
   });
