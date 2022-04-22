7e30cea4c64d3f0188e38647c72a8ec7b93a0d0c
Raman Hundal <ramanhundal@Ramans-MacBook-Pro.local>
2022-04-21 20:46:48 -0700

    Add content script function.

--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,1 @@
-(() => {
-  const fetchBookmarks = () => {};
-
-  const addNewBookmarkEventHandler = () => {};
-
-  const newVideoLoaded = () => {};
-
-  chrome.runtime.onMessage.addListener((obj, sender, response) => {
-    response();
-  });
-})();
-
-const getTime = t => {
-  var date = new Date(0);
-  date.setSeconds(t);
-
-  return date.toISOString().substr(11, 8);
-};
+(() => {})();
