062835bf0843720b674394d23fe42816188dc502
CarstenAtBinderupPro <125389077+CarstenAtBinderupPro@users.noreply.github.com>
2023-02-20 19:57:42 +0100

    bp

--- background.js
+++ background.js
@@ -1,1 +1,21 @@
+console.log("D")
+//Hmm - det lort virker ikke - ved ikke hvorfor - har fulgt videoen 
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+    console.log("A")
+    if (tab.url && tab.url.includes("youtube.com/watch")) {
+        const queryParameters = tab.url.split("?")[1];
+        const urlParameters = new URLSearchParams(queryParameters);
+        console.log(urlParameters);
 
+        chrome.tabs.sendMessage(tabId, {
+            type: "NEW",
+            videoId: urlParameters.get("v")
+        });
+    }
+    console.log("B")
+})
+console.log("E")
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    console.log("A")
+})
--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,15 @@
-(() => {})();
+(() => {
+    let youtubeLeftControls, youtubePlayer;
+    let currentVideo = "";
+    console.log("C")
+
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const {type, value, videoId} = obj;
+
+        if (type === "NEW") {
+            currentVideo = videoId;
+            newVideoLoaded();
+
+        }
+    })
+})();
